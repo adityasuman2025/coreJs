730fa5161cf2cb143c548d4afb754797673bf197
adityasuman2025 <adityasuman2025@gmail.com>
2022-09-14 11:09:50 +0530

    memoizeIt

--- memo.js
+++ memo.js
@@ -17,6 +17,32 @@ const fibonnaci = (function() {
 
     return fibb;
 })()
+// let ans = fibonnaci(10);
+// console.log("ans", ans);
 
-let ans = fibonnaci(10);
-console.log("ans", ans)
+function memoizeIt(func) {
+    let res = {};
+
+    return function(...args) {
+        let key = JSON.stringify(args);
+        if (!(key in res)) {
+            res[key] = func.call(this, ...args)
+        }
+
+        return res[key];
+    }
+}
+
+function heavyFunc(a, b) {
+    for (let i=0; i<=1000000000; i++) {}
+    return a+b;
+}
+
+const memoizedHeavyFunc = memoizeIt(heavyFunc);
+console.time("first call");
+memoizedHeavyFunc(1, 2);
+console.timeEnd("first call");
+
+console.time("second call");
+memoizedHeavyFunc(1, 2);
+console.timeEnd("second call");
