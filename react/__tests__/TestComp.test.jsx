de19ba05455cd2b37b7e94263c56cb81631a9646
adityasuman2025 <adityasuman2025@gmail.com>
2023-09-15 13:44:05 +0530

    mocking api & data in tests

--- react/__tests__/TestComp.test.jsx
+++ react/__tests__/TestComp.test.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import TestComp from '../src/TestComp';
 
-test('renders button correctly', () => {
+
+global.fetch = jest.fn(url => {
+    if (url === 'https://api.example.com/user') {
+        return Promise.resolve({
+            json: () => Promise.resolve({ name: 'John Doe', email: 'john@example.com' }),
+        });
+    } else if (url === 'https://api.example.com/posts') {
+        return Promise.resolve({
+            json: () => Promise.resolve([{ id: 1, title: 'Post 1' }, { id: 2, title: 'Post 2' }]),
+        });
+    }
+});
+
+test('renders button correctly', async () => {
     const handleClick = jest.fn();
     const { getByText } = render(<TestComp text="Click Me" onClick={handleClick} />);
 
@@ -10,4 +23,15 @@ test('renders button correctly', () => {
     fireEvent.click(button);
 
     expect(handleClick).toHaveBeenCalledTimes(1);
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+
+    const nameElement = await screen.findByText('John Doe');
+    const emailElement = screen.getByText('Email: john@example.com');
+
+    expect(nameElement).toBeInTheDocument();
+    expect(emailElement).toBeInTheDocument();
+
+    expect(screen.getByText('Post 1')).toBeInTheDocument();
+    expect(screen.getByText('Post 2')).toBeInTheDocument();
 });
--- react/src/TestComp.jsx
+++ react/src/TestComp.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function TestComp({ text, onClick }) {
-    return <button onClick={onClick}>{text}</button>;
+    const [userData, setUserData] = useState(null);
+    const [postList, setPostList] = useState([]);
+
+    useEffect(() => {
+        // API call to fetch user data
+        fetch('https://api.example.com/user')
+            .then(response => response.json())
+            .then(data => setUserData(data))
+            .catch(error => console.error('Error fetching user data:', error));
+
+        // API call to fetch user's posts
+        fetch('https://api.example.com/posts')
+            .then(response => response.json())
+            .then(data => setPostList(data))
+            .catch(error => console.error('Error fetching user posts:', error));
+    }, []);
+
+    return (
+        <div>
+            {
+                userData ? (
+                    <div>
+                        <h2>{userData.name}</h2>
+                        <p>Email: {userData.email}</p>
+                    </div>
+                ) : <div>Loading...</div>
+            }
+
+            <ul>
+                {postList.map(post => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+
+            <button onClick={onClick}>{text}</button>
+        </div>
+    );
 }
 
 export default TestComp;
