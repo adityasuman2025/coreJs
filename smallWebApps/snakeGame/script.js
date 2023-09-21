0161e0a91db8d465bef6e3ff27d63152709c6934
adityasuman2025 <adityasuman2025@gmail.com>
2023-09-21 18:57:56 +0530

    full fledged snake game done

--- smallWebApps/snakeGame/script.js
+++ smallWebApps/snakeGame/script.js
@@ -2,9 +2,12 @@ const DIMN = 30;
 const ARROW_UP = "ArrowUp", ARROW_DOWN = "ArrowDown", ARROW_LEFT = "ArrowLeft", ARROW_RIGHT = "ArrowRight";
 const KEYS = [ARROW_UP, ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT];
 
-let score = 0;
+
+let score = 1;
 let snakeMovementInterval;
 let snakeMovementDrctn, snakeCurrPosI = DIMN / 2, snakeCurrPosJ = DIMN / 2;
+let snakeBody = [[snakeCurrPosI, snakeCurrPosJ]]; // first element (0th index) - tail , last element (len-1 index) - head
+
 const randomFoodPos = getRandomFoodPosition();
 let foodCurrPosI = randomFoodPos[0], foodCurrPosJ = randomFoodPos[1];
 
@@ -36,43 +39,50 @@ function startSnakeMovement() {
     snakeMovementInterval = setInterval(() => {
         switch (snakeMovementDrctn) {
             case ARROW_UP: {
-                if (snakeCurrPosI - 1 >= 0) {
-                    snakeCurrPosI--;
-                } else handleSnakeDied(); // snake hit the boundary, hence died/game over
+                if (snakeCurrPosI - 1 >= 0) snakeCurrPosI--;
+                else return handleSnakeDied(); // snake hit the boundary, hence died/game over
 
                 break;
             }
             case ARROW_DOWN: {
-                if (snakeCurrPosI + 1 < DIMN) {
-                    snakeCurrPosI++;
-                } else handleSnakeDied(); // snake hit the boundary, hence died/game over
+                if (snakeCurrPosI + 1 < DIMN) snakeCurrPosI++;
+                else return handleSnakeDied(); // snake hit the boundary, hence died/game over
 
                 break;
             }
             case ARROW_LEFT: {
-                if (snakeCurrPosJ - 1 >= 0) {
-                    snakeCurrPosJ--;
-                } else handleSnakeDied(); // snake hit the boundary, hence died/game over
+                if (snakeCurrPosJ - 1 >= 0) snakeCurrPosJ--;
+                else return handleSnakeDied(); // snake hit the boundary, hence died/game over
 
                 break;
             }
             case ARROW_RIGHT: {
-                if (snakeCurrPosJ + 1 < DIMN) {
-                    snakeCurrPosJ++;
-                } else handleSnakeDied(); // snake hit the boundary, hence died/game over
+                if (snakeCurrPosJ + 1 < DIMN) snakeCurrPosJ++;
+                else return handleSnakeDied(); // snake hit the boundary, hence died/game over
 
                 break;
             }
         }
 
+        // checking if snake collided with itself
+        for (let k = 0; k < snakeBody.length; k++) {
+            if (snakeCurrPosI === snakeBody[k][0] && snakeCurrPosJ === snakeBody[k][1]) return handleSnakeDied();
+        }
+        // checking if snake collided with itself
+
+        snakeBody.push([snakeCurrPosI, snakeCurrPosJ]); // after snake's movement, pushing that cell to snake body // it will become snake's new head
 
         // snake collided with food
         if ((foodCurrPosI === snakeCurrPosI) && (foodCurrPosJ === snakeCurrPosJ)) {
             renderScore(++score); // increasing and rendering the updated score
 
-            // giving new position to food
+            // putting food at a new position, as snake ate food at current position
             const randomFoodPos = getRandomFoodPosition();
             foodCurrPosI = randomFoodPos[0], foodCurrPosJ = randomFoodPos[1];
+
+            // if snake collided with food then its size will increase so not removing snake's tail, first element (0th index) of snake's body array
+        } else {
+            snakeBody.shift(); // otherwise removing tail as the snake has moved one step forward
         }
 
         renderBoard();
@@ -92,11 +102,8 @@ function getRandomFoodPosition() {
     let i = Math.floor(Math.random() * (DIMN - 0 + 1)) + 0;
     let j = Math.floor(Math.random() * (DIMN - 0 + 1)) + 0;
 
-    while (i === snakeCurrPosI) { // food can't be at snake's position
+    while ((i === snakeCurrPosI) && (j === snakeCurrPosJ)) { // food can't be at snake's position
         i = Math.floor(Math.random() * (DIMN - 0 + 1)) + 0;
-    }
-
-    while (j === snakeCurrPosJ) { // food can't be at snake's position
         j = Math.floor(Math.random() * (DIMN - 0 + 1)) + 0;
     }
 
@@ -112,7 +119,6 @@ function renderBoard() {
     const tempEle = document.createElement("div");
 
     for (let i = 0; i < DIMN; i++) {
-
         const rowEle = document.createElement("div");
         rowEle.classList.add("row");
 
@@ -120,7 +126,12 @@ function renderBoard() {
             const cellEle = document.createElement("div");
             cellEle.classList.add("cell");
 
-            if (i === snakeCurrPosI && j === snakeCurrPosJ) cellEle.classList.add("snake"); // snake cell
+            // snake cells
+            for (let k = 0; k < snakeBody.length; k++) {
+                if (i === snakeBody[k][0] && j === snakeBody[k][1]) cellEle.classList.add("snake");
+            }
+            // snake cells
+
             if (i === foodCurrPosI && j === foodCurrPosJ) cellEle.classList.add("food"); // food cell
 
             rowEle.append(cellEle);
