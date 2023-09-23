4b6e15537889e86a2771cd1b2006551a6b364fb7
adityasuman2025 <adityasuman2025@gmail.com>
2023-09-23 14:10:28 +0530

    currying

--- currying.js
+++ currying.js
@@ -24,42 +24,8 @@ const ansClosure = addClosureThree(5);
 
 
 
-//currySum(1)(2)(3,4)(5,6,7)(8)(9,10)....()
-//1+2-3+4-5+6-7+8-9+10
-
-let isSum = true;
-function curryAltSum(...args1) {
-    return function(...args2) {
-        let sum = args1[0];
-        args1.forEach((item, index) => {
-            if (index != 0) {
-                if (isSum) {
-                    sum += item;
-                } else {
-                    sum -= item;
-                }
-                isSum = !isSum
-            }
-        });
-
-        args2.forEach((item, index) => {
-            if (isSum) {
-                sum += item;
-            } else {
-                sum -= item;
-            }
-            isSum = !isSum
-        });
-
-        return args2.length ? curryAltSum(sum) : sum
-    }
-}
-
-const ans = curryAltSum(1)(2)(3, 4)(5, 6, 7)(8)(9, 10)()
-console.log("ans", ans)
-
-
 // sum(1)(2)(3)....()
+// 1+2+3+4...
 function sum(x) {
     return function(y) {
         return y ? sum(x + y) : x
@@ -68,3 +34,33 @@ function sum(x) {
 
 const sumVal = sum(1)(2)(3)(4)(5)(6)();
 // console.log("sumVal", sumVal);
+
+
+
+// curryAltSum(1)(2)(3, 4)(5, 6, 7)(8)(9, 10)...()
+// 1+2-3+4-5+6-7+8-9+10
+let isSum = true;
+function curryAltSum(...args1) {
+    let sum = args1[0];
+
+    return function(...args2) {
+        for (let i = 1; i < args1.length; i++) {
+            if (isSum) sum += args1[i]
+            else sum -= args1[i]
+
+            isSum = !isSum;
+        }
+
+        for (let i = 0; i < args2.length; i++) {
+            if (isSum) sum += args2[i]
+            else sum -= args2[i]
+
+            isSum = !isSum;
+        }
+
+        return args2.length ? curryAltSum(sum) : sum
+    }
+}
+
+const ans = curryAltSum(1)(2)(3, 4)(5, 6, 7)(8)(9, 10)()
+console.log("ans", ans)
