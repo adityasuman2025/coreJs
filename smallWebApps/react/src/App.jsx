ec0d644677aba3c47d6737b91084deb1e1d07c05
adityasuman2025 <adityasuman2025@gmail.com>
2023-10-24 14:45:57 +0530

    CountDownTimer

--- smallWebApps/react/src/App.jsx
+++ smallWebApps/react/src/App.jsx
@@ -1,13 +1,15 @@
 import CurrencyInput from "./CurrencyInput";
 import OTPInput from "./OTPInput";
 import AutoCompleteWrapper from "./AutoCompleteWrapper";
+import CountDownTimer from "./CountDownTimer";
 
 function App() {
     return (
         <>
             {/* <CurrencyInput /> */}
             {/* <OTPInput /> */}
-            <AutoCompleteWrapper />
+            {/* <AutoCompleteWrapper /> */}
+            <CountDownTimer />
         </>
     )
 }
