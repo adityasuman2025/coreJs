d20e36d6d7a3df85e9e46a3e0c6e1c0e648b6b81
adityasuman2025 <adityasuman2025@gmail.com>
2023-10-26 14:29:38 +0530

    async await changes

--- asyncAwait.js
+++ asyncAwait.js
@@ -9,3 +9,34 @@ async function yo() {
 }
 
 console.log(yo())
+
+
+function takeTime() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            resolve("done");
+        }, 2000);
+    });
+}
+
+
+async function biro() {
+    const greet = "hello world";
+    const resp = await takeTime();
+    console.log("resp", resp);
+
+    // takeTime().then(resp => {
+    //     console.log("resp", resp);
+    // });
+
+    console.log("greet", greet)
+}
+
+async function yoBiro() {
+    console.log("nyc");
+
+    await biro();
+
+    console.log("pik");
+}
+yoBiro()
