f1f53e43f07ceeca132ea9e57b5a20c5e9a04060
adityasuman2025 <adityasuman2025@gmail.com>
2023-11-09 21:32:49 +0530

    memo for async functions

---
+++ memoAsync.js
@@ -0,0 +1,36 @@
+function memoizeIt(func) {
+    let cache = {};
+
+    return async function(...args) {
+        const key = JSON.stringify(args);
+
+        if (!cache.hasOwnProperty(key)) {
+            cache[key] = func(...args);
+            func(...args)
+                .then(res => {
+                    cache[key] = res;
+                });
+        }
+
+        return cache[key];
+    }
+}
+
+function compute(a, b) {
+    console.log('compute')
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 100)
+    })
+}
+
+
+const memoizeAdd = memoizeIt(compute);
+memoizeAdd(1, 2).then(res => {
+    console.log(res);
+});
+
+memoizeAdd(1, 2).then(res => {
+    console.log(res);
+});
