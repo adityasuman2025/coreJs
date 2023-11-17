05ab42948a4ed78f84391a4d889c5206fa34189d
adityasuman2025 <adityasuman2025@gmail.com>
2023-11-17 14:08:35 +0530

    JSON.parse done

--- JSON.parse.js
+++ JSON.parse.js
@@ -1,74 +1,99 @@
 // https://bigfrontend.dev/problem/implement-JSON-parse
 
-function parse(str) {
-    if (str === '') throw Error()
-    if (str[0] === "'") throw Error()
-    if (str === '[]') return [];
-    if (str === '{}') return {};
-    if (str === 'null') return null;
-    if (str === 'true') return true;
-    if (str === 'false') return false;
-    if (str[0] === '"') return str.slice(1, -1);
-    if (+str === +str) return Number(str);
+function parse(jsonString) {
+    let index = 0;
 
-    if (str[0] === '{') {
-        return str.slice(1, -1).split(',').reduce((acc, item) => {
-            const index = item.indexOf(':');
-            const key = item.slice(0, index)
-            const value = item.slice(index + 1);
-            acc[parse(key)] = parse(value);
+    function parseValue() {
+        const currentChar = jsonString[index];
 
-            return acc;
-        }, {});
+        if (currentChar === '{') {
+            return parseObject();
+        } else if (currentChar === '[') {
+            return parseArray();
+        } else if (currentChar === '"') {
+            return parseString();
+        } else if (currentChar === 't' || currentChar === 'f') {
+            return parseBoolean();
+        } else if (currentChar === 'n') {
+            return parseNull();
+        } else {
+            return parseNumber();
+        }
     }
 
-    if (str[0] === '[') return str.slice(1, -1).split(',').map((value) => parse(value));;
-}
+    function parseObject() {
+        let obj = {};
+        index++; // skip '{'
 
+        while (jsonString[index] !== '}') {
+            const key = parseString();
+            index++; // skip ':'
+            const value = parseValue();
+            obj[key] = value;
 
-
-const ARR_OBJ = ["{", "[",];
-function parse(jsonString) {
-    jsonString = jsonString.trim();
-
-    const isArr = jsonString[0] === "[";
-    const res = isArr ? [] : {};
-
-    jsonString = jsonString.substring(1, jsonString.length - 1);
-
-    const jsonStrArr = jsonString.split(",");
-    jsonStrArr.forEach((keyVal, idx) => {
-        console.log("keyVal", keyVal)
-        if (ARR_OBJ.includes(keyVal)) {
-            console.log('here')
-            keyVal = keyVal + "," + jsonStrArr[idx + 1];
-            console.log("keyVal", keyVal)
-        } else {
-            const keyValArr = keyVal.split(":");
-            const key = keyValArr[0].substring(1, keyValArr[0].length - 1);
-            const val = keyValArr[1].substring(1, keyValArr[1].length - 1);
-
-            if (ARR_OBJ.includes(val[0])) {
-                res[key] = parse(val);
-            } else {
-                res[key] = val;
+            if (jsonString[index] === ',') {
+                index++; // skip ','
             }
         }
-    });
 
-
-    return res;
-}
-
-function splitByFirstColon(str) {
-    let end1 = 0;
-
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i];
-        if (char === ":") {
-            end1 = i - 1;
-        }
+        index++; // skip '}'
+        return obj;
     }
 
-    return [str.substring(0, end1), str.substring(end1, str.length)]
+    function parseArray() {
+        let arr = [];
+        index++; // skip '['
+
+        while (jsonString[index] !== ']') {
+            const value = parseValue();
+            arr.push(value);
+
+            if (jsonString[index] === ',') {
+                index++; // skip ','
+            }
+        }
+
+        index++; // skip ']'
+        return arr;
+    }
+
+    function parseString() {
+        let str = '';
+        index++; // skip '"'
+
+        while (jsonString[index] !== '"') {
+            str += jsonString[index];
+            index++;
+        }
+
+        index++; // skip '"'
+        return str;
+    }
+
+    function parseBoolean() {
+        const boolStr = jsonString.slice(index, index + 4);
+        index += 4; // skip 'true' or 'false'
+        return boolStr === 'true';
+    }
+
+    function parseNull() {
+        index += 4; // skip 'null'
+        return null;
+    }
+
+    function parseNumber() {
+        let numStr = '';
+
+        while (/[\d.+\-eE]/.test(jsonString[index])) {
+            numStr += jsonString[index];
+            index++;
+        }
+
+        return parseFloat(numStr);
+    }
+
+    return parseValue();
 }
+
+const jsonStr = '{"yo":"biro","a":{"b":{"c":[1],"nyc":"pik"}}}';
+console.log(parse(jsonStr))
