8abb1967b252517df3a61e0b0711f3631005d24f
adityasuman2025 <adityasuman2025@gmail.com>
2026-05-24 11:06:15 +0530

    otp input

--- smallWebAppsInReact/src/OTPInput.jsx
+++ smallWebAppsInReact/src/OTPInput.jsx
@@ -1,54 +1,48 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react"
 
-const OTP_FIELDS = [0, 1, 2, 3, 4, 5];
+function isNumber(str) {
+    return Number.isFinite(Number(str)) && str.trim() !== "";
+}
+
+const OTP_LENGTH = 6;
 export default function OTPInput() {
     const otpFieldRefs = useRef([]);
-    const [otp, setOtp] = useState(OTP_FIELDS.reduce((acc, i) => ({ ...acc, [i]: "" }), {}));
+    const [otp, setOtp] = useState(new Array(OTP_LENGTH).fill(""));
 
-    function handleOtpChange(e, otpFieldIdx) {
-        let val = (e.key)?.trim();
-        val = filterInput(val, otpFieldIdx);
+    useEffect(() => {
+        if ("filter", otp.filter(val => isNumber(val)).length === OTP_LENGTH) console.log("submit otp")
+    }, [otp]);
 
-        setOtp(prev => ({ ...prev, [otpFieldIdx]: val }));
-    }
+    function handleOtpChange(e, inpIdx) {
+        const value = e.key.trim();
 
-    function filterInput(key, otpFieldIdx) {
-        if (key && !isNaN(Number(key))) {
-            focusField("next", otpFieldIdx);
-            return key;
-        } else if (key === "Backspace") {
-            focusField("prev", otpFieldIdx);
-            return "";
-        } else if (key === "ArrowRight") {
-            focusField("next", otpFieldIdx);
-        } else if (key === "ArrowLeft") {
-            focusField("prev", otpFieldIdx);
+        if (isNumber(value)) {
+            setOtp(prev => prev.map((val, idx) => idx === inpIdx ? value : val)); // setting up value in the state
+
+            if (inpIdx + 1 < OTP_LENGTH) otpFieldRefs.current?.[inpIdx + 1].focus(); // making the next otp input in focus
+        } else {
+            if (value === "ArrowRight" && inpIdx + 1 < OTP_LENGTH) otpFieldRefs.current?.[inpIdx + 1].focus(); // making the next otp input in focus
+            else if (value === "ArrowLeft" && inpIdx - 1 >= 0) otpFieldRefs.current?.[inpIdx - 1].focus(); // making the prev otp input in focus
+            else if (value === "Backspace" && inpIdx >= 0) {
+                setOtp(prev => prev.map((val, idx) => idx === inpIdx ? "" : val)); // clearing up value in the state
+                if (inpIdx - 1 >= 0) otpFieldRefs.current?.[inpIdx - 1].focus(); // making the prev otp input in focus
+            }
         }
-
-        return otp[otpFieldIdx];
-    }
-
-    function focusField(direction, otpFieldIdx) {
-        let newFocus = otpFieldIdx;
-
-        if (direction === "prev") {
-            newFocus = otpFieldIdx - 1 < 0 ? otpFieldIdx : otpFieldIdx - 1
-        } else if (direction === "next") {
-            newFocus = otpFieldIdx + 1 >= OTP_FIELDS.length ? otpFieldIdx : otpFieldIdx + 1
-        }
-
-        otpFieldRefs.current[newFocus].focus();
     }
 
     return (
         <div id="content">
             <div id="otp">
                 {
-                    OTP_FIELDS.map(i => (
+                    new Array(OTP_LENGTH).fill(0).map((_, i) => (
                         <input
                             ref={el => { otpFieldRefs.current[i] = el }}
-                            key={i} type="text" className="otpInput"
-                            maxLength={1} value={otp[i]} onKeyUp={(e) => handleOtpChange(e, i)} onChange={() => { }}
+                            key={i} className="otpInput"
+                            type="text"
+                            maxLength={1}
+                            value={otp[i]}
+                            onKeyUp={(e) => handleOtpChange(e, i)}
+                            onChange={() => { }}
                             autoFocus={i === 0}
                         />
                     ))
