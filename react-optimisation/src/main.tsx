919d9ce3914af7b328a276affea515e3fdc3b5a5
adityasuman2025 <adityasuman2025@gmail.com>
2026-06-16 19:45:25 +0530

    react memo

--- react-optimisation/src/main.tsx
+++ react-optimisation/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Tab from './Tab.jsx'
 
 createRoot(document.getElementById('root')!).render(<App />)
