2447d536b60923e7d7abf2bb488ee7692ce30e9a
adityasuman2025 <adityasuman2025@gmail.com>
2026-07-16 21:16:44 +0530

    flatu removed

---
+++ practice.js
@@ -0,0 +1,13 @@
+export default function useDebounce(value, delay) {
+    const [debVal, setDebVal] = useState(value);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebVal(value);
+        }, delay);
+
+        return () => clearTimeout(timer);
+    }, [value, delay]);
+
+    return debVal;
+}
